7b906ded04a1bf88f3647748f5cdccc0956ab0eb
Stewart ilondanga <stilondanga@gmail.com>
2018-12-06 20:31:20 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var navigate = (function() {
+/*var navigate = (function() {
 	$('.dd').toggle();
 	$('.dd_btn').click(function() {
 		var dataName = $(this).attr('data-name');
@@ -6,3 +6,22 @@ var navigate = (function() {
 		$('.' + dataName).toggle();
 	});
 })();
+
+var clears = document.querySelectorAll(".clear");
+var inputs = document.querySelectorAll("input");
+
+for (let i=0; i<3; i++) {
+  inputs[i].addEventListener("input", function(e) {
+    if (e.target.value.length > 0) {
+      clears[i].style.display = "block";
+    }
+    else {
+      clears[i].style.display = "none";
+    }
+  });
+
+  clears[i].addEventListener("click", function() {
+    inputs[i].value = "";
+    this.style.display = "none";
+  });
+}
